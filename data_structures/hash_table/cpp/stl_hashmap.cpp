ee48014c07a2b88b7a7578459ac441a35dd129b3
Ashish Lakhani <depashlakhani@gmail.com>
2017-10-21 13:57:54 +0530

    Create stl_hashmap.cpp
    STL Implementation of hashMap Added

---
+++ data_structures/hash_table/cpp/stl_hashmap.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  map<int,int> m; // Initializing a Hash Map having a key value pair of Integers
+  m[10]=20; // key = 10, value = 20
+  m[40]=90; // key = 40, value= 90
+  
+  cout<<m[40]<<endl // Prints 90
+  
+ return 0;
+}
